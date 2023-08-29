cb75c350617fac0e7bd68d32a9d867058a534de4
RodrigoAC1 <rocavalheiro1@gmail.com>
2023-08-29 20:18:48 -0300

    main

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
+import Title from '@/components/Title'
 import Image from 'next/image'
-import Title from '../components/Title';
-
 
 export default function Home() {
-  return (
+  return ( //JSX
     <>
       <nav className="flex p-4 bg-green-700">
         <ul className="flex gap-20">
@@ -28,25 +27,26 @@ export default function Home() {
             </a>
           </li>
         </ul>
+
       </nav>
 
-      <Title>Em alta</Title>
-      
-      <div id="card" className='flex flex-col w-40 justify-center m-2'>
-        <img className='rounded' scr="https://place-hold.it/150x220/666" alt=""/>
-        <span className='font-bold text-center line-clamp-1'>Nome da musica do card</span>
-        </div>
-        <span>6.0</span>
+     <Title>em alta</Title>
+
+      <div id="card" className='flex flex-col w-40 justify-center items-center m-2'>
+        <img className='rounded' src="https://place-hold.it/150x220/666" alt="" />
+        <span className='font-bold text-center line-clamp-1'>Nome da musica</span>
         <div>
+          <span>6.0</span>
+        </div>
         <a href="#" className='bg-red-600 py-2 w-full rounded text-center'>
-          Detalhes
+          detalhes
         </a>
       </div>
 
-      <Title>Lançamentos</Title>
-
-
+     <Title>Novas</Title>
+     <Title>favoritos</Title>
+   
     </>
-  );
+   
+  )
 }
-
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 export default function Title({children}){
     return(
-        <h2 className="text-x1 font-semibold border-red-600 border-1-8 m-2 p1-2">
-         {children}
+        <h2 className="text-xl font-semibold border-red-600 border-l-8 m-2 pl-2">
+            {children}
         </h2>
     )
 }
